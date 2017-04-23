5e6e18026a9400c15283073e7019ea899e0945e3
Pauric Holleran <pauric.holleran@gmail.com>
2017-04-23 14:03:30 +0000

    updated API call

--- js/index.js
+++ js/index.js
@@ -12,7 +12,7 @@ function celciusToFahrenheit(celcius) {
   return fahrenheit;
 }
 
-function runSecondAjax(latitude, longitude, country) {
+function runSecondAjax(latitude, longitude) {
   var weatherUrl = 'http://api.openweathermap.org/data/2.5/weather?lat=' + latitude + '&lon=' + longitude + '&APPID=c61f567cd175ce7a781517bc769dab46' + '&units=metric';
   $.ajax({
     url: weatherUrl,
@@ -22,9 +22,9 @@ function runSecondAjax(latitude, longitude, country) {
       fahrenheit = Math.round(celciusToFahrenheit(celcius));
       var city = data.name;
       var iconid = data.weather[0].id;
-      $(document).ready(function () {
+      $(document).ready(function() {
 
-        $("#city").html(city + ", " + country);
+        $("#city").html(city);
         $("#summary").html(summary);
         $("#tempicon").toggleClass("wi-owm-" + iconid, true);
         $("#temp").html("Temperature: " + celcius + "&deg;");
@@ -38,60 +38,36 @@ function runSecondAjax(latitude, longitude, country) {
   });
 }
 
-$(document).ready(function () {
+function getLocation() {
+  navigator.geolocation.getCurrentPosition(showPosition);
+  console.log("hi");
+}
+
+function showPosition(position) {
+  var latitude = position.coords.latitude;
+  var longitude = position.coords.longitude;
+  console.log(lat);
+  console.log(position);
+  //country = data.country;
+  runSecondAjax(latitude, longitude);
+  //getWeather(lat, lon);
+}
+
+$(document).ready(function() {
 
   getLocation();
 
-  function getLocation() {
-    navigator.geolocation.getCurrentPosition(showPosition);
-    console.log("hi");
-  }
-
-  function showPosition(position) {
-    var lat = position.coords.latitude;
-    var lon = position.coords.longitude;
-    console.log(lat);
-    //getWeather(lat, lon);
-    console.log(position);
-  }
-
-  $("#toggleTemp").on('click', function () {
+  $("#toggleTemp").on('click', function() {
     if (displayTempCel == true) {
       $("#toggleTemp").html("F");
       $("#temp").html("Temperature: " + fahrenheit + "&deg;");
       displayTempCel = false;
-    } else {
+    }
+    else {
       $("#toggleTemp").html("C");
       $("#temp").html("Temperature: " + celcius + "&deg;");
       displayTempCel = true;
     }
   });
-});
 
-$(document).ready(function () {
-
-  //Get Co-ordinates
-  var longitude;
-  var latitude;
-  var myUrl = "http://ip-api.com/json/?callback=";
-  var country;
-  $.ajax({
-    url: myUrl,
-    success: function success(data) {
-      longitude = data.lon;
-      latitude = data.lat;
-      country = data.country;
-      console.log(data);
-      console.log(longitude);
-      console.log(latitude);
-      runSecondAjax(latitude, longitude, country);
-    },
-    error: function error(jqXHR, errorMessage, errorThrown) {
-      //console.log("error");
-      //console.log(errorMessage);
-      //console.log(errorThrown);
-      //console.log(jqXHR);
-      //$("#summary").html(jqXHR.responseText);
-    }
-  });
 });
